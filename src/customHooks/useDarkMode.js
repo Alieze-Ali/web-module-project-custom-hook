4dbe25c3e411cba3f95271f3545ceea9da8689bb
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-10-05 20:58:24 -0700

    added dark mode custom hook

---
+++ src/customHooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = (initalValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', initalValue)
+    return[darkMode, setDarkMode];
+}
